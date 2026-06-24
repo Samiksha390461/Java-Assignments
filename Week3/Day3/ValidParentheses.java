d236adc0eab1cc55e547086f59cbe349112666bc
Samiksha kumari <samikshakumari461@gmail.com>
2026-06-24 16:05:38 +0530

    Update ValidParentheses.java

--- Week3/Day3/ValidParentheses.java
+++ Week3/Day3/ValidParentheses.java
@@ -1,1 +1,30 @@
+class Solution {
+    public boolean isValid(String s) {
+        ArrayDeque<Character> expected = new ArrayDeque<>();
 
+        for (char ch : s.toCharArray()) {
+
+            switch (ch) {
+                case '(':
+                    expected.push(')');
+                    break;
+
+                case '{':
+                    expected.push('}');
+                    break;
+
+                case '[':
+                    expected.push(']');
+                    break;
+
+                default:
+                    if (expected.isEmpty() || expected.pop() != ch) {
+                        return false;
+                    }
+            }
+        }
+
+        return expected.isEmpty();
+        
+    }
+}
