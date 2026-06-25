930bc20c44c975a1534cbe862240a00242d6c1ff
Samiksha kumari <samikshakumari461@gmail.com>
2026-06-25 14:11:20 +0530

    Update EvaluateReversePolishNotion.java

--- Week3/Day4/EvaluateReversePolishNotion.java
+++ Week3/Day4/EvaluateReversePolishNotion.java
@@ -1,1 +1,49 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+         int[] stack = new int[tokens.length];
+        int top = -1;
 
+        for (String token : tokens) {
+
+            if (isOperator(token)) {
+
+                int second = stack[top--];
+                int first = stack[top--];
+
+                stack[++top] = calculate(first, second, token);
+
+            } else {
+                stack[++top] = Integer.parseInt(token);
+            }
+        }
+
+        return stack[top];
+    }
+
+    private boolean isOperator(String s) {
+        return s.length() == 1 &&
+                (s.charAt(0) == '+' ||
+                 s.charAt(0) == '-' ||
+                 s.charAt(0) == '*' ||
+                 s.charAt(0) == '/');
+    }
+
+    private int calculate(int a, int b, String op) {
+
+        switch (op.charAt(0)) {
+            case '+':
+                return a + b;
+
+            case '-':
+                return a - b;
+
+            case '*':
+                return a * b;
+
+            default:
+                return a / b;
+        }
+
+        
+    }
+}
